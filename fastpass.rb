1917a9d88c6e9e36d23aa180a16f9e4501850957
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-08 13:43:26 +0000

    Release v1.9

--- fastpass.rb
+++ fastpass.rb
@@ -3,12 +3,11 @@ require 'formula'
 class Fastpass < Formula
   desc ""
   homepage ""
-  version "1.8"
-  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v1.8/fastpass"
-  sha256 "644fb0168695f538ae6afb733017c16023b178c3d5beb3e8a7dff122e9dc4d27"
+  version "1.9"
+  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v1.9/fastpass"
+  sha256 "81c081d04e36e485b2fe1b71b9d27122382fd16f32b2959d5cb78313d1e38ea1"
   def install
-    old_path = '/usr/local/bin/fastpass'
-    File.delete(old_path) if File.exist?(old_path)
+    File.delete(path_to_file) if File.exist?(path_to_file)
     bin.install 'fastpass'
   end
 end
