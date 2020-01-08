ec24b1579151628b11841e38565e7b54ce6fa807
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-08 13:36:44 +0000

    Set version

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 set -eux
 METALS_VERSION=$1
 VERSION=$2
+TAG=v$2
 coursier bootstrap org.scalameta:metals_2.12:$METALS_VERSION -r sonatype:snapshots --main scala.meta.internal.pantsbuild.BloopPants -o fastpass -f
-hub release create -a fastpass -m "Fastpass $VERSION" $VERSION
+hub release create -a fastpass -m "Fastpass $TAG" $TAG
 BINARY=$(mktemp -d)/fastpass
-FASTPASS_URL=https://github.com/olafurpg/homebrew-fastpass/releases/download/$VERSION/fastpass
+FASTPASS_URL=https://github.com/olafurpg/homebrew-fastpass/releases/download/$TAG/fastpass
 curl -o $BINARY $FASTPASS_URL
 SHA=$(shasum -a 256 $BINARY | awk '{ print $1 }')
 
@@ -14,6 +15,7 @@ require 'formula'
 class Fastpass < Formula
   desc ""
   homepage ""
+  version "$VERSION"
   url "$FASTPASS_URL"
   sha256 "$SHA"
   def install
@@ -23,5 +25,5 @@ class Fastpass < Formula
 end
 EOF
 git add .
-git commit -am "Release $VERSION"
+git commit -am "Release $TAG"
 
