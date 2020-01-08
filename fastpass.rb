495b8fdfb469de346389217f12c118e8e9c7e3ed
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-08 13:43:08 +0000

    Fix unresolved variable

--- fastpass.rb
+++ fastpass.rb
@@ -7,7 +7,8 @@ class Fastpass < Formula
   url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v1.8/fastpass"
   sha256 "644fb0168695f538ae6afb733017c16023b178c3d5beb3e8a7dff122e9dc4d27"
   def install
-    File.delete(path_to_file) if File.exist?(path_to_file)
+    old_path = '/usr/local/bin/fastpass'
+    File.delete(old_path) if File.exist?(old_path)
     bin.install 'fastpass'
   end
 end
