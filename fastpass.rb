3914880da3ccdffb5f5768d3e47fd9a4eed5cf7b
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-08 13:16:21 +0000

    Add unused URL

--- fastpass.rb
+++ fastpass.rb
@@ -1,6 +1,10 @@
 require 'formula'
 
 class Fastpass < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/olafurpg/homebrew-fastpass/archive/v1.0.zip"
+  sha256 "bc77b18d4b780520c4c26a37051d8becbebd370de7dc42ced53256a96e42e2fa"
   depends_on 'coursier/formulas/coursier'
   def install
     system 'coursier',
