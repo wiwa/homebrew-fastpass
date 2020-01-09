31546bce981c15f08d4dc11c1d76b3e5c805238d
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-09 19:51:56 +0000

    Release v20

--- build-tar.sh
+++ build-tar.sh
@@ -1,10 +1,36 @@
 #!/usr/bin/env zsh
 set -eux
-METALS_VERSION=${METALS_VERSION:-0.7.7-SNAPSHOT}
+METALS_VERSION=${METALS_VERSION:-0.7.6+724-16691b3e-SNAPSHOT}
 BLOOP_VERSION=${BLOOP_VERSION:-1.4.0-RC1}
-TAG=$1
+VERSION="$1"
+TAG="v$1"
 NATIVE=$(~/.jabba/bin/jabba which --home graal-custom@19.3)/bin/native-image
-$NATIVE -cp $(cs fetch org.scalameta:metals_2.12:$METALS_VERSION -p) --initialize-at-build-time --no-server --no-fallback --allow-incomplete-classpath -H:ReflectionConfigurationFiles=$(pwd)/reflection.json scala.meta.internal.pantsbuild.BloopPants bin/fastpass
-$NATIVE -cp $(cs fetch ch.epfl.scala:bloopgun_2.12:$BLOOP_VERSION -p) --initialize-at-build-time --no-server --no-fallback --allow-incomplete-classpath -H:ReflectionConfigurationFiles=$(pwd)/reflection.json bloop.bloopgun.Bloopgun bin/bloop
-tar -czvf fastpass.tar.gz bin/
-hub release create -a fastpass.tar.gz -m "Fastpass $TAG" $TAG
+CLASSPATH=$(cs fetch org.scalameta:metals_2.12:$METALS_VERSION -p -r sonatype:snapshots)
+# $NATIVE -cp $CLASSPATH --initialize-at-build-time --no-server --no-fallback --allow-incomplete-classpath -H:ReflectionConfigurationFiles=$(pwd)/reflection.json scala.meta.internal.pantsbuild.BloopPants bin/fastpass
+# $NATIVE -cp $(cs fetch ch.epfl.scala:bloopgun_2.12:$BLOOP_VERSION -p) --initialize-at-build-time --no-server --no-fallback --allow-incomplete-classpath -H:ReflectionConfigurationFiles=$(pwd)/reflection.json bloop.bloopgun.Bloopgun bin/bloop
+TAR=fastpass.tar.gz
+tar -czvf $TAR bin/
+hub release create -a $TAR -m "Fastpass $TAG" $TAG
+FASTPASS_URL=https://github.com/olafurpg/homebrew-fastpass/releases/download/$TAG/$TAR
+SHA=$(curl -L $FASTPASS_URL | shasum -a 256 | awk '{ print $1 }')
+
+cat > fastpass.rb <<EOF
+require 'formula'
+
+class Fastpass < Formula
+  desc "Metals $VERSION"
+  homepage ""
+  version "$VERSION"
+  url "$FASTPASS_URL"
+  sha256 "$SHA"
+  depends_on "coursier/formulas/coursier"
+  depends_on "scalacenter/bloop/bloop"
+  def install
+    bin.install "bin/fastpass"
+  end
+end
+EOF
+git add .
+git commit -am "Release $TAG"
+git push origin master
+brew upgrade fastpass
--- fastpass.rb
+++ fastpass.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Fastpass < Formula
-  desc "Metals 1.17"
+  desc "Metals 20"
   homepage ""
-  version "1.17"
-  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v1.17/fastpass"
-  sha256 "21492671c8f14a1f7e9c0fe4fbe50e5d7c1d8f455fde5f0025a923eea0cad9f7"
+  version "20"
+  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v20/fastpass.tar.gz"
+  sha256 "c836764a03ce406bb4119d4e6ce234b93771c3d44ca352c5602b7f7c2b80305d"
   depends_on "coursier/formulas/coursier"
   depends_on "scalacenter/bloop/bloop"
   def install
-    bin.install "fastpass"
+    bin.install "bin/fastpass"
   end
 end
