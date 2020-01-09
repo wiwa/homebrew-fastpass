b2313092958ee996f7d88e68df09dd8ee9b51432
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-09 17:10:58 +0000

    Release v1.17

--- fastpass.rb
+++ fastpass.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Fastpass < Formula
-  desc "Metals 1.16"
+  desc "Metals 1.17"
   homepage ""
-  version "1.16"
-  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v1.16/fastpass"
-  sha256 "e66f86a99a1a130307b1f50735373a322a7a04089c91dfcd2d891c72b5d04877"
+  version "1.17"
+  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v1.17/fastpass"
+  sha256 "21492671c8f14a1f7e9c0fe4fbe50e5d7c1d8f455fde5f0025a923eea0cad9f7"
   depends_on "coursier/formulas/coursier"
   depends_on "scalacenter/bloop/bloop"
   def install
