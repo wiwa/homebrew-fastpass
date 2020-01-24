65c83bf5b568f617db47f7636d2d74fca939c74c
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-24 19:29:24 +0000

    Release v24

--- build-tar.sh
+++ build-tar.sh
@@ -6,6 +6,8 @@ VERSION="$1"
 TAG="v$1"
 NATIVE=$(~/.jabba/bin/jabba which --home graal-custom@19.3)/bin/native-image
 CLASSPATH=$(cs fetch org.scalameta:metals_2.12:$METALS_VERSION -p -r sonatype:snapshots)
+rm -rf bin
+mkdir bin
 $NATIVE -cp $CLASSPATH \
   --initialize-at-build-time \
   --no-server \
@@ -16,8 +18,17 @@ $NATIVE -cp $CLASSPATH \
   -H:ReflectionConfigurationFiles=$(pwd)/reflection.json \
   scala.meta.internal.pantsbuild.BloopPants \
   bin/fastpass
-# $NATIVE -cp $(cs fetch ch.epfl.scala:bloopgun_2.12:$BLOOP_VERSION -p) --initialize-at-build-time --no-server --no-fallback --allow-incomplete-classpath -H:ReflectionConfigurationFiles=$(pwd)/reflection.json bloop.bloopgun.Bloopgun bin/bloop
+$NATIVE -cp $(cs fetch ch.epfl.scala:bloopgun_2.12:$BLOOP_VERSION -p) \
+  --initialize-at-build-time \
+  --no-server \
+  --no-fallback \
+  --allow-incomplete-classpath \
+  -H:EnableURLProtocols=https \
+  -H:ReflectionConfigurationFiles=$(pwd)/reflection.json \
+  bloop.bloopgun.Bloopgun bin/bloop
 TAR=fastpass.tar.gz
+file bin/fastpass
+file bin/bloop
 tar -czvf $TAR bin/
 hub release create -a $TAR -m "Fastpass $TAG" $TAG
 FASTPASS_URL=https://github.com/olafurpg/homebrew-fastpass/releases/download/$TAG/$TAR
--- fastpass.rb
+++ fastpass.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Fastpass < Formula
-  desc "Metals 23"
+  desc "Metals 24"
   homepage ""
-  version "23"
-  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v23/fastpass.tar.gz"
-  sha256 "ef405b55f4d088f48107973bde2fd351a1a2662fedae51e6b4addea9951753d4"
+  version "24"
+  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v24/fastpass.tar.gz"
+  sha256 "fd77f2087bba7f2d173b9bd1a52b7dc2e562eee4175115b8a34d09c5625688cd"
   depends_on "scalacenter/bloop/bloop"
   def install
     bin.install "fastpass"
