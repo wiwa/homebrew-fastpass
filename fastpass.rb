41f04c56f7a02a303387279b0fef759bafe7bd19
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-02-21 11:03:52 +0000

    Release v30

--- fastpass.rb
+++ fastpass.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Fastpass < Formula
-  desc "Metals 29"
+  desc "Metals 30"
   homepage ""
-  version "29"
-  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v29/fastpass.tar.gz"
-  sha256 "88045b86c1103dbb94dd2b1db153ca357fd478dff4618db5f1d94110186e42f6"
+  version "30"
+  url "https://github.com/olafurpg/homebrew-fastpass/releases/download/v30/fastpass.tar.gz"
+  sha256 "5bf86b70bb04b4b0103db8a33c0d6f2a0e22e624cb335ef6549e2b018d0262ee"
   depends_on "scalacenter/bloop/bloop"
   def install
     bin.install "fastpass"
