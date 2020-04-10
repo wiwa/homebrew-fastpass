ce8e2eca62445f06ddf4c6e0dc77a96996b7634c
Win Wang <winw@twitter.com>
2020-04-10 01:36:41 -0400

    Add fastpass-config to classpath for native-image

--- link-fastpass.sh
+++ link-fastpass.sh
@@ -1,7 +1,7 @@
 set -eux
 VERSION=$1
 NATIVE=$(~/.jabba/bin/jabba which --home graalvm@19.3.1)/bin/native-image
-$NATIVE -cp $(cs fetch org.scalameta:metals_2.12:$VERSION -p -r sonatype:snapshots) \
+$NATIVE -cp $(cs fetch org.scalameta:metals_2.12:$VERSION -p -r sonatype:snapshots):fastpass-config \
   --initialize-at-build-time \
   --initialize-at-run-time=scala.meta.internal.pantsbuild,metaconfig \
   --no-server \
